9be908bc971ffbb36dd31cd8169f2ca7153ed051
Sahil Chowdhury <sahilc0708@gmail.com>
2021-05-29 11:30:47 +0530

    First Commit:Api setup done with just a dummy page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Cards = (props) => {
+    console.log(props);
+    return (
+        <h1>Cards</h1>
+    )
+}
+
+export default Cards;
