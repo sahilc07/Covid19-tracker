68005102020a538292ca1a19f4358cc7b4336aea
Sahil Chowdhury <sahilc0708@gmail.com>
2021-05-29 22:25:41 +0530

    Third Commit: Line Chart added

--- src/api/index.js
+++ src/api/index.js
@@ -8,6 +8,23 @@ export const fetchData = async () => {
 
         return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
+        console.log(error);
+    }
+}
 
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }))
+
+        return modifiedData;
+
+    } catch (error) {
+        console.log(error);
     }
 }
