46d7a5c886317df099db2f208f2a3b5490f32027
Sahil Chowdhury <sahilc0708@gmail.com>
2021-05-30 18:55:33 +0530

    Added Barchart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import { fetchDailyData } from '../../api'
 import { Line, Bar } from 'react-chartjs-2'
 import styles from './Chart.module.css'
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 
     const [dailyData, setDailyData] = useState([]);
 
@@ -35,10 +35,29 @@ const Chart = () => {
                 }}
             /> : null
     );
+    const barChart = (
+        confirmed
+            ? (
+                <Bar
+                    data={{
+                        labels: ['Infected', 'Recovered', 'Deaths'],
+                        datasets: [{
+                            label: 'People',
+                            backgroundColor: ['rgba(255,0,0, 0.5)', 'rgba(0,255,0, 0.5)', 'rgba(0,0,0, 0.5)',],
+                            data: [confirmed.value, recovered.value, deaths.value]
+                        }]
+                    }}
+                    options={{
+                        legend: { display: false },
+                        title: { display: true, text: `Current state in ${country}` }
+                    }}
+                />
+            ) : null
+    )
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
